76e9f8b9ef8e302eb799863020d544573faf136d
Dione Betanin <dione.silva@agrotis.com>
2019-01-31 18:04:03 -0200

    feature: Add body parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require("body-parser");
 const axios = require('axios');
 
 const app = express();
 
-const port = process.env.APP_PORT || process.env.PORT || 3000;
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -59,12 +60,11 @@ app.all('/forward', async (req, res, next) => {
     let timeoutCount = 0;
 
     try {
-        
         const request = {
             method: req.method,
             url: req.headers.to,
             headers,
-            data: req.body && JSON.parse(req.body),
+            data: req.body,
         };
 
         while (true) {
@@ -87,6 +87,8 @@ app.all('/forward', async (req, res, next) => {
 
     next();
 });
+
+const port = process.env.APP_PORT || process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Worker listening at port ${port}`);
